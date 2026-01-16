49cc5c492d5e130e24e0142c290d9f8b45434010
Levin Bänninger <levin@baenninger.me>
2026-01-16 21:45:52 +0100

    fix: use correct import order

--- src/modules/overview/ui/views/overview.tsx
+++ src/modules/overview/ui/views/overview.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AlertCircle } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
